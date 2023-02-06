98e37ca677e9cff6562b89ac659472e6a57aca7c
Grace Akpan <opps4lrng@gmail.com>
2023-02-06 00:45:38 -0700

    added service-worker files

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { createLogger } from "redux-logger";
 import "tachyons";
 
 import App from "./containers/App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import { requestRobots, searchRobots } from "./reducers";
 
 import "./index.css";
@@ -31,6 +32,11 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
